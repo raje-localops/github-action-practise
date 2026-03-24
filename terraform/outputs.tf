0e99d8615cceb88b0a5c141e4ae3c0f47347c59b
raje <rajeshwari.k@localops.co>
2026-03-24 11:03:51 +0530

    Create outputs.tf

---
+++ terraform/outputs.tf
@@ -0,0 +1,19 @@
+output "cluster_name" {
+  description = "EKS cluster name"
+  value       = module.eks.cluster_name
+}
+
+output "cluster_endpoint" {
+  description = "EKS cluster endpoint"
+  value       = module.eks.cluster_endpoint
+}
+
+output "cluster_version" {
+  description = "Kubernetes version"
+  value       = module.eks.cluster_version
+}
+
+output "kubeconfig_command" {
+  description = "Command to update kubeconfig"
+  value       = "aws eks update-kubeconfig --region ${var.aws_region} --name ${module.eks.cluster_name}"
+}
